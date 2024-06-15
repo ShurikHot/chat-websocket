bf9895ba1af2eb77848b8ff723c94580f8d63a06
shurikhot <shurikhot2@gmail.com>
2024-06-15 14:05:36 +0300

    avatar

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -3,7 +3,7 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import { Link, useForm, usePage } from '@inertiajs/vue3';
+import {Link, useForm, usePage} from '@inertiajs/vue3';
 
 defineProps({
     mustVerifyEmail: {
@@ -12,6 +12,7 @@ defineProps({
     status: {
         type: String,
     },
+
 });
 
 const user = usePage().props.auth.user;
@@ -19,7 +20,15 @@ const user = usePage().props.auth.user;
 const form = useForm({
     name: user.name,
     email: user.email,
+    avatar: user.avatar,
+    avatar_file: null,
 });
+
+const handleFileChange = (event) => {
+    form.avatar_file = event.target.files[0];
+    /*console.log(form.avatar);
+    console.log(event.target.files[0]);*/
+};
 </script>
 
 <template>
@@ -33,69 +42,84 @@ const form = useForm({
         </header>
 
         <form @submit.prevent="form.patch(route('profile.update'))" class="mt-6 space-y-6">
-            <div>
-                <InputLabel for="name" value="Name" />
+            <div class="flex space-x-4">
+                <div class="w-1/2">
+                    <div>
+                        <InputLabel for="name" value="Name" />
 
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
+                        <TextInput
+                            id="name"
+                            type="text"
+                            class="mt-1 block w-full"
+                            v-model="form.name"
+                            required
+                            autofocus
+                            autocomplete="name"
+                        />
 
-                <InputError class="mt-2" :message="form.errors.name" />
-            </div>
+                        <InputError class="mt-2" :message="form.errors.name" />
+                    </div>
 
-            <div>
-                <InputLabel for="email" value="Email" />
+                    <div class="my-6">
+                        <InputLabel for="email" value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
+                        <TextInput
+                            id="email"
+                            type="email"
+                            class="mt-1 block w-full"
+                            v-model="form.email"
+                            required
+                            autocomplete="username"
+                        />
 
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+                        <InputError class="mt-2" :message="form.errors.email" />
+                    </div>
 
-            <div v-if="mustVerifyEmail && user.email_verified_at === null">
-                <p class="text-sm mt-2 text-gray-800">
-                    Your email address is unverified.
-                    <Link
-                        :href="route('verification.send')"
-                        method="post"
-                        as="button"
-                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Click here to re-send the verification email.
-                    </Link>
-                </p>
+                    <div v-if="mustVerifyEmail && user.email_verified_at === null">
+                        <p class="text-sm mt-2 text-gray-800">
+                            Your email address is unverified.
+                            <Link
+                                :href="route('verification.send')"
+                                method="post"
+                                as="button"
+                                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                Click here to re-send the verification email.
+                            </Link>
+                        </p>
 
-                <div
-                    v-show="status === 'verification-link-sent'"
-                    class="mt-2 font-medium text-sm text-green-600"
-                >
-                    A new verification link has been sent to your email address.
+                        <div
+                            v-show="status === 'verification-link-sent'"
+                            class="mt-2 font-medium text-sm text-green-600"
+                        >
+                            A new verification link has been sent to your email address.
+                        </div>
+                    </div>
+
+                    <div class="flex items-center gap-4">
+                        <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+
+                        <Transition
+                            enter-active-class="transition ease-in-out"
+                            enter-from-class="opacity-0"
+                            leave-active-class="transition ease-in-out"
+                            leave-to-class="opacity-0"
+                        >
+                            <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                        </Transition>
+                    </div>
                 </div>
-            </div>
 
-            <div class="flex items-center gap-4">
-                <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+                <div class="w-1/2">
+                    <InputLabel for="avatar" value="Avatar" />
+                    <div class="w-[100px] h-[100px] border mx-auto">
+                        <img :src="form.avatar" alt="">
+                    </div>
+                    <div class="flex mt-3">
+                        <input type="file" @change="handleFileChange" class="px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 mx-auto">
+                    </div>
 
-                <Transition
-                    enter-active-class="transition ease-in-out"
-                    enter-from-class="opacity-0"
-                    leave-active-class="transition ease-in-out"
-                    leave-to-class="opacity-0"
-                >
-                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
-                </Transition>
+                </div>
             </div>
         </form>
     </section>
